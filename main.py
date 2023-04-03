60446d2a86d7232d499d12f586dcbaf1e538d8e1
Alan Alrechah <alan.richeh@gmail.com>
2023-04-03 20:58:47 +0200

    fix the prediction pipeline

--- main.py
+++ main.py
@@ -1,210 +1,43 @@
-from enum import Enum
-from numpy import NaN
+import os
 from core.constants import SampleOption, SamplingMethod
-import data
-import core.prediction_methods.config.settings as pred
-import core.prediction_methods.config.metrics as met
-import core.prediction_methods.methods as methods
-from core.prediction_methods.utils.logfile import LogFile
+from core.prediction_methods.predict import train_and_predict
 from core.concept_drift import drift_detection
-import pm4py
 from config import root_directory
-import pandas as pd
-import numpy as np
-from subprocess import PIPE, Popen
-
 from utils import import_csv_eventlog, import_xes_eventlog
 
 
-class TrainMethod(Enum):
-    SDL = "sdl"
-    LIN = "lin"
-    PASQUADIBISCEGLIE = "pasquadibisceglte"
+def predict_original():
+    """
+    Train and test the next event prediction on the original log without detecting drifts
+    """
+    log_train = "sww_train"
+    log_test = "sww_test"
+    train_file_path = os.path.join(root_directory, "data", "input", "csv", log_train + ".csv")
+    test_file_path = os.path.join(root_directory, "data", "input", "csv", log_test + ".csv")
+    
+    accuracy, f1_score = train_and_predict(log_train, train_file_path, log_test, test_file_path)
+
+    print("Accuracy: ", accuracy)
+    print("F1 Score: ", f1_score)
     
 
-
-def predict_after_detection(method: TrainMethod = TrainMethod.SDL, train_file:str = "sample", test_file:str = "sww_test"):
-
-    if method is TrainMethod.SDL:
-        #start with new data
-        train_data = data.get_data(train_file)
-
-        #select predection method
-        settings = pred.SDL
-
-        #select the percentage of training with is always 100%
-        settings.train_percentage = 100
-        #prepare data with predection method settings
-        train_data.prepare(settings)
-
-        #getting the prediction method parameters
-        method = methods.get_prediction_method("SDL")
-
-        
-        #get the test data
-        test_data = data.get_data(test_file)
-        
-        #select predection method for testing
-        test_settings = pred.SDL
-        
-        #select the train percentage for test data which will always be 0%
-        settings.train_percentage = 0
-        #prepare test data for testing
-        test_data.prepare(test_settings)
-
-        #train the model with the training data
-        model = method.train(train_data.train)
-
-        #test model with the testing data
-        results = method.test(model, test_data.test_orig)
-
-        #calculate accuracy
-        accuracy = met.ACCURACY.calculate(results)
-
-    elif method is TrainMethod.LIN:
-        #start with new data
-        train_data = data.get_data(train_file)
-
-        #select predection method
-        settings = pred.LIN
-
-        #select the percentage of training with is always 100%
-        settings.train_percentage = 100
-        #prepare data with predection method settings
-        train_data.prepare(settings)
-
-        #getting the prediction method parameters
-        method = methods.get_prediction_method("LIN")
-
-        
-        #get the test data
-        test_data = data.get_data(test_file)
-        
-        #select predection method for testing
-        test_settings = pred.LIN
-        
-        #select the train percentage for test data which will always be 0%
-        settings.train_percentage = 0
-        #prepare test data for testing
-        test_data.prepare(test_settings)
-
-        #train the model with the training data
-        model = method.train(train_data.train)
-
-        #test model with the testing data
-        results = method.test(model, test_data.test_orig)
-
-        #calculate accuracy
-        accuracy = met.ACCURACY.calculate(results)
+def predict_sampled():
+    """
+    Train and test the next event prediction on the sampled log after detecting drifts
+    """
+    original_train_log = "sww_train"
+    #detect drifts
+    sampled_log = drift_detection.get_sampled_log(original_train_log, "100", True, 0.6, SamplingMethod.PRIORITY_LAST, SampleOption.CASES_FROM_COUNT_EVENTS)
     
-    elif method is TrainMethod.PASQUADIBISCEGLIE:
-         #start with new data
-        train_data = data.get_data(train_file)
-
-        #normalize date and time for train data
-        for idx, row in train_data.logfile.data.iterrows():
-            time = pd.to_datetime(row["completeTime"])
-            train_data.logfile.data.at[idx, 'completeTime'] = time.strftime('%Y/%m/%d %H:%M:%S.%f')
-
-        #select predection method
-        settings = pred.PASQUADIBISCEGLIE
-
-        #select the percentage of training with is always 100%
-        settings.train_percentage = 100
-        #prepare data with predection method settings
-        train_data.prepare(settings)
-
-        #getting the prediction method parameters
-        method = methods.get_prediction_method("PASQUADIBISCEGLIE")
-
-        
-        #get the test data
-        test_data = data.get_data(test_file)
-
-        #nomalize the date and time for test data
-        for idx, row in test_data.logfile.data.iterrows():
-            time = pd.to_datetime(row["completeTime"])
-            test_data.logfile.data.at[idx, 'completeTime'] = time.strftime('%Y/%m/%d %H:%M:%S.%f')
-        
-        #select predection method for testing
-        test_settings = pred.PASQUADIBISCEGLIE
-        
-        #select the train percentage for test data which will always be 0%
-        settings.train_percentage = 0
-        #prepare test data for testing
-        test_data.prepare(test_settings)
-
-        #train the model with the training data
-        model = method.train(train_data.train)
-
-        #test model with the testing data
-        results = method.test(model, test_data.test_orig)
-
-        #calculate accuracy
-        accuracy = met.ACCURACY.calculate(results)    
+    log_train = "sampled"
+    log_test = "sww_test"
+    train_file_path = os.path.join(root_directory, "data", "output", log_train + ".csv")
+    test_file_path = os.path.join(root_directory, "data", "input", "csv", log_test + ".csv")
     
-    
-    
-    print("accuracy: ", accuracy)
-    
+    accuracy, f1_score = train_and_predict(log_train, train_file_path, log_test, test_file_path)
 
-
-def predict():
-    
-    #data_object = data.get_data("IOR5k", time="time:timestamp", case="case:concept:name", activity="concept:name", resource="org:resource")
-    #data_object = data.get_data("sample")
-    data_object = data.get_data("sww_train")
-    #data_object = data.get_data("sample_sdl")
-
-    for idx, row in data_object.logfile.data.iterrows():
-        dd = pd.to_datetime(row["completeTime"])
-        data_object.logfile.data.at[idx, 'completeTime'] = dd.strftime('%Y/%m/%d %H:%M:%S.%f')
-    # data_object.logfile.data.style.format({"completeTime": lambda t: t.strftime("%Y/%m/%d %H:%M:%S.%f")})
-    # data_object.logfile.data["completeTime"] = pd.to_datetime(data_object.logfile.data["completeTime"])
-    # data_object.logfile.data["completeTime"] = data_object.logfile.data["completeTime"].dt.strftime('%Y/%m/%d %H:%M:%S.%f')
-
-    #settings = pred.LIN
-    #settings = pred.SDL
-    settings = pred.PASQUADIBISCEGLIE
-
-    settings.train_percentage = 100
-    data_object.prepare(settings)
-
-    #m = methods.get_prediction_method("LIN")
-    #m = methods.get_prediction_method("SDL")
-    m = methods.get_prediction_method("PASQUADIBISCEGLIE")
-
-    test_data = data.get_data("sww_test")
-
-    for idx, row in test_data.logfile.data.iterrows():
-        dd = pd.to_datetime(row["completeTime"])
-        test_data.logfile.data.at[idx, 'completeTime'] = dd.strftime('%Y/%m/%d %H:%M:%S.%f')
-    #test_settings = pred.LIN
-    #test_settings = pred.SDL
-    test_settings = pred.PASQUADIBISCEGLIE
-    
-    settings.train_percentage = 0
-    test_data.prepare(test_settings)
-    
-    model = m.train(data_object.train)
-
-    results = m.test(model, test_data.test_orig)
-
-    accuracy = met.ACCURACY.calculate(results)
-
-    print("accuracy: ", accuracy)
-
-
-# def detect(customized_sampling: bool = False):
-#     if customized_sampling:
-#         #apply drift detection
-#         all_sublogs = concept_drift.apply_drift_detection(log_name="sww_90", delta=0.002)
-#         #apply general sampling
-#         result_log = concept_drift.general_sampling(all_sublogs, probabilites=[1, 0, 0, 0], case_percent=0.6, all_cases=False)
-#         # export result log
-#         concept_drift.export_sampled_log(result_log)
-#     else:
-#         concept_drift.detect_drifts(use_saved_sublogs=False, sampling_method=concept_drift.SamplingMethod.UNIFORM, log_name="sww_90", over_sampling=True, all_cases=False)
+    print("Accuracy: ", accuracy)
+    print("F1 Score: ", f1_score)
 
 
 def import_event_log():
@@ -232,15 +65,16 @@ def import_event_log():
     print(result)
 
 
-
 def detect_drift_generate_sample_log():
     """
     Detects the drifts in a log then generate a sample log depending on a sample method and a sample option
     """
     drift_detection.get_sampled_log("sww_90", "100", True, 0.6, SamplingMethod.PRIORITY_LAST, SampleOption.CASES_FROM_COUNT_EVENTS)
 
+
+
 if __name__ == '__main__':
-    detect_drift_generate_sample_log()
-    #predict()
-    #detect()
-    #predict_after_detection(method= TrainMethod.SDL)
+    #detect_drift_generate_sample_log()
+    #import_event_log()
+    #predict_sampled()
+    predict_original()
